3ae4261303c794e5d2d044b2f0fdc4051ec19006
Gilberto <con-valniria.bandeira@telemetria-supergasbras.com.br>
2020-06-14 00:37:43 -0300

    commit inicial

---
+++ src/main/java/com/carros/services/CarroService.java
@@ -0,0 +1,32 @@
+package com.carros.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.carros.domain.Carro;
+import com.carros.repository.CarroRepository;
+
+
+@Service
+public class CarroService {
+
+	@Autowired
+	private CarroRepository repository;
+	
+	public Iterable<Carro> getCarros(){
+		
+		return repository.findAll();
+	
+	}
+
+	public Optional<Carro> getCarroById(Long id) { 
+		return repository.findById(id);
+	}
+
+	public Iterable<Carro> getCarrosByTipo(String tipo) {
+		return repository.findByTipo(tipo);
+	}
+	
+}
